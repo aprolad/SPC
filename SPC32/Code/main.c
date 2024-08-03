ad50ab49eb5b4f68aaf9e75c2f8eb8f9c104760b
aprolad@gmail.com <aprolad@gmail.com>
2024-08-03 20:53:51 +0300

    GPU + IO

--- SPC32/Code/main.c
+++ SPC32/Code/main.c
@@ -1,24 +1,34 @@
+
 int main()
 {
-    int *vram_ptr = 0;
-    unsigned long addr = (unsigned long)vram_ptr;
-    // Bit shift the address
-    unsigned long shifted_addr = (addr + 1) << 18; // Right shift by 1 bit
-    // Cast the shifted address back to a pointer
+
+    unsigned long shifted_addr = 1 << 18;
+    unsigned long key_addr = (1 << 19) + shifted_addr;
+    volatile int *key_ptr = (int *)key_addr;
     volatile int *shifted_ptr = (int *)shifted_addr;
     *shifted_ptr = 0x05ffff10;
-
     volatile int t =  0x05ffff10;
     shifted_ptr += 1;
-    for (;;)
+    while (1)
     {
-        shifted_ptr -= 1;
-        *shifted_ptr = t;
-        t += 0x01ffffff;
-        shifted_ptr += 1;
-        *shifted_ptr = ~0;
+            shifted_ptr -= 1;
+            *shifted_ptr = t;
+            switch (*key_ptr){ 
+            case 1:
+                t += 0x10000000;
+                break;    
+            case 2:
+                t = ((t - 0x01000000) & 0x0f000000) +  (t & 0xf0ffffff); 
+                break;    
+            case 4:
+                t -= 0x10000000;
+                break;    
+            case 8:
+                t = ((t + 0x01000000) & 0x0f000000) +  (t & 0xf0ffffff); 
+                break;    
+            }                     
+            shifted_ptr += 1;
+            *shifted_ptr = ~0;
     }
-
-
 
 }
